e3949f2b0e27501f7bd0113419019ec6fc6917ff
liuri66 <2875585471@qq.com>
2024-04-25 20:12:32 +0800

    init project

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # test1
 测试
 hello word
+第一次学这个内容
---
+++ release/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Plugin/Preset files are not allowed to export objects, only functions. In C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\babel-preset-es2015\\\\lib\\\\index.js\\n    at createDescriptor (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:160:11)\\n    at createDescriptor.next (<anonymous>)\\n    at step (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:261:32)\\n    at evaluateAsync (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:291:5)\\n    at C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:44:11\\n    at Array.forEach (<anonymous>)\\n    at Function.async (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:43:15)\\n    at Function.all (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:216:13)\\n    at Generator.next (<anonymous>)\\n    at createDescriptors (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:102:41)\\n    at createDescriptors.next (<anonymous>)\\n    at createPresetDescriptors (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:96:17)\\n    at createPresetDescriptors.next (<anonymous>)\\n    at C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\functional.js:39:27\\n    at Generator.next (<anonymous>)\\n    at mergeChainOpts (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:350:34)\\n    at mergeChainOpts.next (<anonymous>)\\n    at chainWalker (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:316:14)\\n    at chainWalker.next (<anonymous>)\\n    at loadFileChain (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:191:24)\\n    at loadFileChain.next (<anonymous>)\\n    at buildRootChain (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:108:31)\\n    at buildRootChain.next (<anonymous>)\\n    at loadPrivatePartialConfig (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\partial.js:72:62)\\n    at loadPrivatePartialConfig.next (<anonymous>)\\n    at loadPartialConfig (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\partial.js:115:25)\\n    at loadPartialConfig.next (<anonymous>)\\n    at step (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:269:25)\\n    at C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:273:13\\n    at async.call.result.err.err (C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:223:11)\\n    at C:\\\\Users\\\\zheng\\\\Desktop\\\\test1\\\\node_modules\\\\gensync\\\\index.js:37:40\");\n\n//# sourceURL=webpack://test1/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
